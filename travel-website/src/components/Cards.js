d6abb99c5ad2a7f060bcbf3cb9216bcebd0f995f
Jean Silva <jheanrds@gmail.com>
2021-04-11 01:40:54 -0300

    Add new components

--- travel-website/src/components/Cards.js
+++ travel-website/src/components/Cards.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import CardItem from './CardItem'
+import './Cards.css'
+import travelImage2 from '../images/img-2.jpg'
+import travelImage3 from '../images/img-3.jpg'
+import travelImage4 from '../images/img-4.jpg'
+import travelImage8 from '../images/img-8.jpg'
+import travelImage9 from '../images/img-9.jpg'
 
 function Cards() {
   return (
@@ -8,7 +14,38 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem />
+            <CardItem 
+              src={travelImage9}
+              text="Explore the hidden waterfall deep inside the amazon Jungle"
+              label="Adventures"
+              path="/services"
+            />
+            <CardItem 
+              src={travelImage2}
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Luxury"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem 
+              src={travelImage3}
+              text="Set sail in the Atlantic Ocean visiting Uncharted Waters"
+              label="Mystery"
+              path="/services"
+            />
+            <CardItem 
+              src={travelImage4}
+              text="Experience Football on Top og the Himilayan Mountains"
+              label="Adventure"
+              path="/products"
+            />
+            <CardItem 
+              src={travelImage8}
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="Adrenaline"
+              path="/sign-up"
+            />
           </ul>
         </div>
       </div>
--- travel-website/src/components/HeroSection.js
+++ travel-website/src/components/HeroSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button } from './Button';
 import '../App.css'
 import './HeroSection.css'
-import videoBanner from '../videos/video-2.mp4'
+import videoBanner from '../videos/video-1.mp4'
 
 function HeroSection() {
   return (
